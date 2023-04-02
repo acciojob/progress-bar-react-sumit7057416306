657f4297dba1d52a429939823b71e7e273055bad
sumit7057416306 <120789691+sumit7057416306@users.noreply.github.com>
2023-04-02 19:17:08 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,45 @@
 
-import React from "react";
-import './../styles/App.css';
+import React, { useState, useEffect } from "react";
 
 const App = () => {
+  const [progress, setProgress] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setProgress((prevProgress) => {
+        if (prevProgress >= 100) {
+          clearInterval(intervalId);
+          return 100;
+        }
+
+        return prevProgress + 10;
+      });
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h1>Progress Bar</h1>
+      <div id='barOuter'
+        style={{ width: "500px", height: "20px", backgroundColor: "lightgray" }}
+      >
+        <div
+          id='barInner'
+          style={{
+            width: `${progress}%`,
+            height: "100%",
+            backgroundColor: "blue",
+            transition: "width 0.5s ease-in-out",
+          }}
+        />
+      </div>
+      <p>{progress}%</p>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
